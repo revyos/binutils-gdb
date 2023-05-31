56b3743108ff40f5c9d49c06d1452674858ed7be
Gianluca Guida <gianluca@rivosinc.com>
2023-05-31 17:28:56 +0100

    RISC-V: Support Zacas extension.
    https://github.com/riscvarchive/riscv-zacas/releases/tag/v1.0
    
    The Zacas extension introduce compare-and-swap instructions to operate
    on 32-bit, 64-bit and 128-bit (RV64 only) data values.
    
    It introduces three new instructions:
      - amocas.w (32-bit CAS)
      - amocas.d (64-bit CAS)
      - amocas.q (128-bit CAS, RV64 only)
    
    Like other AMOs in the A extension, Zacas instructions have '.aq',
    '.rl' and '.aqrl' variations.
    
    bfd/ChangeLog:
    
    	* elfxx-riscv.c (riscv_implicit_subsets): 'A' implied by 'Zacas'.
    	(riscv_supported_std_z_ext): Add 'Zacas' extension.
    	(riscv_multi_subset_supports, riscv_multi_subset_supports_ext):
    	Handle INSN_CLASS_ZACAS case.
    
    gas/ChangeLog:
    
    	* NEWS: Updated.
    	* testsuite/gas/riscv/march-help.l: Updated.
    	* testsuite/gas/riscv/zacas-32.d: New test (RV32).
            * testsuite/gas/riscv/zacas-fail-32.d: Likewise.
    	* testsuite/gas/riscv/zacas-64.d: New test (RV64).
            * testsuite/gas/riscv/zacas-fail-64.d: Likewise.
    	* testsuite/gas/riscv/zacas.s: New test source.
    	* testsuite/gas/riscv/zacas-fail.s: Likewise.
    	* testsuite/gas/riscv/zacas-fail-32.l: New file.
    	* testsuite/gas/riscv/zacas-fail-64.l: Likewise.
    
    include/ChangeLog:
    
    	* include/opcode/riscv.h (INSN_CLASS_ZACAS): New definition.
    	* include/opcode/riscv-opc.h (MATCH_AMOCAS_W, MASK_AMOCAS_W)
    	(MATCH_AMOCAS_D, MASK_AMOCAS_D, MATCH_AMOCAS_Q, MASK_AMOCAS_Q):
    	Likewise.
    	(amocas_w, amocas_d, amocas_q): Declare instructions.
    
    opcodes/ChangeLog:
    
    	* riscv-opc.c (match_rs2_rd_even): New function.
    	(amocas_w, amocas_d, amocas_q, amocas_w.aq)
    	(amocas_d.aq, amocas_q.aq, amocas_w.rl, amocas_d.rl, amocas_q.rl)
    	(amocas_w.aqrl, amocas_d.aqrl, amocas_q.aqrl): Add instructions.

--- bfd/elfxx-riscv.c
+++ bfd/elfxx-riscv.c
@@ -1118,6 +1118,7 @@ static struct riscv_implicit_subset riscv_implicit_subsets[] =
   {"v", "zve64d",	check_implicit_always},
   {"v", "zvl128b",	check_implicit_always},
   {"zabha", "a",	check_implicit_always},
+  {"zacas", "a",	check_implicit_always},
   {"zvfbfmin", "zve32f",	check_implicit_always},
   {"zvfbfwma", "zve32f",	check_implicit_always},
   {"zvfbfwma", "zfbfmin",	check_implicit_always},
@@ -1314,6 +1315,7 @@ static struct riscv_supported_ext riscv_supported_std_z_ext[] =
   {"za128rs",		ISA_SPEC_CLASS_DRAFT,		1, 0,  0 },
   {"zaamo",		ISA_SPEC_CLASS_DRAFT,		1, 0,  0 },
   {"zabha",		ISA_SPEC_CLASS_DRAFT,		1, 0,  0 },
+  {"zacas",		ISA_SPEC_CLASS_DRAFT,		1, 0,  0 },
   {"zalrsc",		ISA_SPEC_CLASS_DRAFT,		1, 0,  0 },
   {"zawrs",		ISA_SPEC_CLASS_DRAFT,		1, 0,  0 },
   {"zfbfmin",		ISA_SPEC_CLASS_DRAFT,		1, 0,  0 },
@@ -2503,6 +2505,8 @@ riscv_multi_subset_supports (riscv_parse_subset_t *rps,
       return riscv_subset_supports (rps, "zaamo");
     case INSN_CLASS_ZABHA:
       return riscv_subset_supports (rps, "zabha");
+    case INSN_CLASS_ZACAS:
+      return riscv_subset_supports (rps, "zacas");
     case INSN_CLASS_ZALRSC:
       return riscv_subset_supports (rps, "zalrsc");
     case INSN_CLASS_ZAWRS:
@@ -2738,6 +2742,8 @@ riscv_multi_subset_supports_ext (riscv_parse_subset_t *rps,
       return "zaamo";
     case INSN_CLASS_ZABHA:
       return "zabha";
+    case INSN_CLASS_ZACAS:
+      return "zacas";
     case INSN_CLASS_ZALRSC:
       return "zalrsc";
     case INSN_CLASS_ZAWRS:
