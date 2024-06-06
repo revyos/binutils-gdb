8eab20e8aee6e1f8a7373a349cc672ed1542ffec
Xiao Zeng <zengxiao@eswincomputing.com>
2024-06-06 15:59:53 +0800

    RISC-V: Add support for Zvfbfwma extension
    This implements the Zvfbfwma extension, as of version 1.0.
    View detailed information in:
    <https://github.com/riscv/riscv-isa-manual/blob/main/src/bfloat16.adoc#zvfbfwma---vector-bf16-widening-mul-add>
    
    1 In spec: "Zvfbfwma requires the Zvfbfmin extension and the Zfbfmin extension."
      1.1 In Embedded    Processor: Zvfbfwma -> Zvfbfmin -> Zve32f
      1.2 In Application Processor: Zvfbfwma -> Zvfbfmin -> V
      1.3 In both scenarios, there are: Zvfbfwma -> Zfbfmin
    
    2 Depending on different usage scenarios, the Zvfbfwma extension may
    depend on 'V' or 'Zve32f'. This patch only implements dependencies in
    scenario of Embedded Processor. This is consistent with the processing
    strategy in Zvfbfmin. In scenario of Application Processor, it is
    necessary to explicitly indicate the dependent 'V' extension.
    
    For relevant information in gcc, please refer to:
    <https://gcc.gnu.org/git/?p=gcc.git;a=commit;h=38dd4e26e07c6be7cf4d169141ee4f3a03f3a09d>
    
    bfd/ChangeLog:
    
    	* elfxx-riscv.c (riscv_multi_subset_supports): Handle Zvfbfwma.
    	(riscv_multi_subset_supports_ext): Ditto.
    
    gas/ChangeLog:
    
    	* NEWS: Updated.
    	* testsuite/gas/riscv/march-help.l: Ditto.
    	* testsuite/gas/riscv/zvfbfwma.d: New test.
    	* testsuite/gas/riscv/zvfbfwma.s: New test.
    
    include/ChangeLog:
    
    	* opcode/riscv-opc.h (MATCH_VFWMACCBF16_VF): Define.
    	(MASK_VFWMACCBF16_VF): Ditto.
    	(MATCH_VFWMACCBF16_VV): Ditto.
    	(MASK_VFWMACCBF16_VV): Ditto.
    	(DECLARE_INSN): New declarations for Zvfbfwma.
    	* opcode/riscv.h (enum riscv_insn_class): Add
    	INSN_CLASS_ZVFBFWMA
    
    opcodes/ChangeLog:
    
    	* riscv-opc.c: Add Zvfbfwma instructions.

--- bfd/elfxx-riscv.c
+++ bfd/elfxx-riscv.c
@@ -1119,6 +1119,8 @@ static struct riscv_implicit_subset riscv_implicit_subsets[] =
   {"v", "zve64d",	check_implicit_always},
   {"v", "zvl128b",	check_implicit_always},
   {"zvfbfmin", "zve32f",	check_implicit_always},
+  {"zvfbfwma", "zve32f",	check_implicit_always},
+  {"zvfbfwma", "zfbfmin",	check_implicit_always},
   {"zvfh", "zvfhmin",	check_implicit_always},
   {"zvfh", "zfhmin",	check_implicit_always},
   {"zvfhmin", "zve32f",	check_implicit_always},
@@ -1311,6 +1313,7 @@ static struct riscv_supported_ext riscv_supported_std_z_ext[] =
   {"zvbb",		ISA_SPEC_CLASS_DRAFT,		1, 0,  0 },
   {"zvbc",		ISA_SPEC_CLASS_DRAFT,		1, 0,  0 },
   {"zvfbfmin",		ISA_SPEC_CLASS_DRAFT,		1, 0,  0 },
+  {"zvfbfwma",		ISA_SPEC_CLASS_DRAFT,		1, 0,  0 },
   {"zvfh",		ISA_SPEC_CLASS_DRAFT,		1, 0,  0 },
   {"zvfhmin",		ISA_SPEC_CLASS_DRAFT,		1, 0,  0 },
   {"zvkb",		ISA_SPEC_CLASS_DRAFT,		1, 0,  0 },
@@ -2552,6 +2555,8 @@ riscv_multi_subset_supports (riscv_parse_subset_t *rps,
       return riscv_subset_supports (rps, "zvbc");
     case INSN_CLASS_ZVFBFMIN:
       return riscv_subset_supports (rps, "zvfbfmin");
+    case INSN_CLASS_ZVFBFWMA:
+      return riscv_subset_supports (rps, "zvfbfwma");
     case INSN_CLASS_ZVKB:
       return riscv_subset_supports (rps, "zvkb");
     case INSN_CLASS_ZVKG:
@@ -2812,6 +2817,8 @@ riscv_multi_subset_supports_ext (riscv_parse_subset_t *rps,
       return _("zvbc");
     case INSN_CLASS_ZVFBFMIN:
       return "zvfbfmin";
+    case INSN_CLASS_ZVFBFWMA:
+      return "zvfbfwma";
     case INSN_CLASS_ZVKB:
       return _("zvkb");
     case INSN_CLASS_ZVKG:
