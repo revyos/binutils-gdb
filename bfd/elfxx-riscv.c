39fd5fd388025ee34ca18c88b3a94c406befac60
Nelson Chu <nelson@rivosinc.com>
2024-06-18 12:48:26 +0800

    BACKPORT: 2.44: RISC-V: Support ssctr/smctr extensions with version 1.0.
    https://github.com/riscv/riscv-control-transfer-records/releases/tag/v1.0
    
    The privileged spec v1.10 already removed the sfence.vm instruction, and the
    encoding of sfence.vm instruction is overlapped with the sctrclr instruction
    of ssctr/smctr.  But since the privileged spec v1.10 already removed the
    sfence.vm, and we no longer support the privileged spec v1.9.1 for now, we
    had to remove the sfence.vm.
    
    bfd/
    	* elfxx-riscv.c (riscv_implicit_subsets): Imply zicsr for ssctr/smctr.
    	(riscv_supported_std_s_ext): Added ssctr/smctr with version 1.0.
    	(riscv_multi_subset_supports): Handle INSN_CLASS for ssctr/smctr.
    	(riscv_multi_subset_supports_ext): Likewise.
    gas/
    	* config/tc-riscv.c (enum riscv_csr_class, riscv_csr_address):
    	Added and handle CSR_CLASS_SSCTR and CSR_CLASS_SMCTR.
    	(riscv_is_priv_insn): Removed SFENCE_VM check.
    	* testsuite/gas/riscv/attribute-14e.d: Removed since sfence.vm is no
    	longer supported since privileged spec v1.10.
    	* testsuite/gas/riscv/attribute-14.s: Likewise.
    	* testsuite/gas/riscv/csr-version-1p10.d: Updated for ssctr/smctr CSRs.
    	* testsuite/gas/riscv/csr-version-1p10.l: Likewise.
    	* testsuite/gas/riscv/csr-version-1p11.d: Likewise.
    	* testsuite/gas/riscv/csr-version-1p11.l: Likewise.
    	* testsuite/gas/riscv/csr-version-1p12.d: Likewise.
    	* testsuite/gas/riscv/csr-version-1p12.l: Likewise.
    	* testsuite/gas/riscv/csr.s: Likewise.
    	* testsuite/gas/riscv/csr-dw-regnums.d: Likewise.
    	* testsuite/gas/riscv/csr-dw-regnums.s: Likewise.
    	* testsuite/gas/riscv/march-help.l: Updated for ssctr/smctr.
    	* testsuite/gas/riscv/smctr-ssctr.d: New testcase for sctr instruction.
    	* testsuite/gas/riscv/smctr-ssctr.s: Likewise.
    include/
    	* opcode/riscv-opc.h: Added encoding macro for sctrclr, but removed
    	encoding macro for sfence.vm since encoding conflict.  Added CSR
    	numbers for ssctr/smctr CSRs.
    	* opcode/riscv.h (enum riscv_insn_class): Added
    	INSN_CLASS_SMCTR_OR_SSCTR for sctrclr.
    opcodes/
    	* riscv-opc.c (riscv_opcodes): Added sctrclr, but removed sfence.vm
    	since encoding conflict.
    
    (cherry picked from commit 1d458f0843141734b73f3a895dee73626fd85cb3)
    Signed-off-by: Han Gao <gaohan@iscas.ac.cn>

--- bfd/elfxx-riscv.c
+++ bfd/elfxx-riscv.c
@@ -1255,6 +1255,7 @@ static struct riscv_implicit_subset riscv_implicit_subsets[] =
   {"smaia", "+ssaia", check_implicit_always},
   {"smcsrind", "+sscsrind", check_implicit_always},
   {"smcntrpmf", "+zicsr", check_implicit_always},
+  {"smctr", "+zicsr", check_implicit_always},
   {"smstateen", "+ssstateen", check_implicit_always},
   {"smepmp", "+zicsr", check_implicit_always},
 
@@ -1262,6 +1263,7 @@ static struct riscv_implicit_subset riscv_implicit_subsets[] =
   {"sscsrind", "+zicsr", check_implicit_always},
   {"sscofpmf", "+zicsr", check_implicit_always},
   {"sscounterenw", "+zicsr", check_implicit_always},
+  {"ssctr", "+zicsr", check_implicit_always},
   {"ssstateen", "+zicsr", check_implicit_always},
   {"sstc", "+zicsr", check_implicit_always},
   {"sstvala", "+zicsr", check_implicit_always},
@@ -1440,6 +1442,7 @@ static struct riscv_supported_ext riscv_supported_std_s_ext[] =
   {"smaia",		ISA_SPEC_CLASS_DRAFT,		1, 0, 0 },
   {"smcsrind",		ISA_SPEC_CLASS_DRAFT,		1, 0, 0 },
   {"smcntrpmf",		ISA_SPEC_CLASS_DRAFT,		1, 0, 0 },
+  {"smctr",		ISA_SPEC_CLASS_DRAFT,		1, 0, 0 },
   {"smepmp",		ISA_SPEC_CLASS_DRAFT,		1, 0, 0 },
   {"smrnmi",		ISA_SPEC_CLASS_DRAFT,		1, 0, 0 },
   {"smstateen",		ISA_SPEC_CLASS_DRAFT,		1, 0, 0 },
@@ -1448,6 +1451,7 @@ static struct riscv_supported_ext riscv_supported_std_s_ext[] =
   {"sscsrind",		ISA_SPEC_CLASS_DRAFT,		1, 0, 0 },
   {"sscofpmf",		ISA_SPEC_CLASS_DRAFT,		1, 0, 0 },
   {"sscounterenw",	ISA_SPEC_CLASS_DRAFT,		1, 0, 0 },
+  {"ssctr",		ISA_SPEC_CLASS_DRAFT,		1, 0, 0 },
   {"ssstateen",		ISA_SPEC_CLASS_DRAFT,		1, 0, 0 },
   {"sstc",		ISA_SPEC_CLASS_DRAFT,		1, 0, 0 },
   {"sstvala",		ISA_SPEC_CLASS_DRAFT,		1, 0, 0 },
@@ -2718,6 +2722,9 @@ riscv_multi_subset_supports (riscv_parse_subset_t *rps,
       return riscv_subset_supports (rps, "zcmp");
     case INSN_CLASS_ZCMT:
       return riscv_subset_supports (rps, "zcmt");
+    case INSN_CLASS_SMCTR_OR_SSCTR:
+      return (riscv_subset_supports (rps, "smctr")
+	      || riscv_subset_supports (rps, "ssctr"));
     case INSN_CLASS_SVINVAL:
       return riscv_subset_supports (rps, "svinval");
     case INSN_CLASS_H:
@@ -2990,6 +2997,8 @@ riscv_multi_subset_supports_ext (riscv_parse_subset_t *rps,
       return "zcmp";
     case INSN_CLASS_ZCMT:
       return "zcmt";
+    case INSN_CLASS_SMCTR_OR_SSCTR:
+      return _("smctr' or `ssctr");
     case INSN_CLASS_SVINVAL:
       return "svinval";
     case INSN_CLASS_H:
