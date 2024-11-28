e9e7096776ed6ea2d984d90fb4da3d78ed058739
Dongyan Chen <chendongyan@isrc.iscas.ac.cn>
2024-11-28 20:35:36 +0800

    BACKPORT: 2.44: RISC-V: Add support for ssdbltrp and smdbltrp extension.
    This implements the ssdbltrp extensons, version 1.0[1] and the smdbltrp
    extensions, version1.0[2].
    
    [1] https://github.com/riscv/riscv-isa-manual/blob/main/src/ssdbltrp.adoc
    [2] https://github.com/riscv/riscv-isa-manual/blob/main/src/smdbltrp.adoc
    
    bfd/ChangeLog:
    
    	* elfxx-riscv.c: Add 'ssdbltrp' and 'smdbltrp' to the list of konwn
    	  standard extensions.
    
    gas/ChangeLog:
    
    	* NEWS: Updated.
    	* testsuite/gas/riscv/imply.d: Ditto.
    	* testsuite/gas/riscv/imply.s: Ditto.
    	* testsuite/gas/riscv/march-help.l: Ditto.
    
    (cherry picked from commit 69a91bcd6eb72ccf5144bbcdee624c481066e5b3)
    Signed-off-by: Han Gao <gaohan@iscas.ac.cn>

--- bfd/elfxx-riscv.c
+++ bfd/elfxx-riscv.c
@@ -1258,6 +1258,7 @@ static struct riscv_implicit_subset riscv_implicit_subsets[] =
   {"smctr", "+zicsr", check_implicit_always},
   {"smstateen", "+ssstateen", check_implicit_always},
   {"smepmp", "+zicsr", check_implicit_always},
+  {"smdbltrp", "+zicsr", check_implicit_always},
 
   {"ssaia", "+zicsr", check_implicit_always},
   {"sscsrind", "+zicsr", check_implicit_always},
@@ -1269,6 +1270,7 @@ static struct riscv_implicit_subset riscv_implicit_subsets[] =
   {"sstvala", "+zicsr", check_implicit_always},
   {"sstvecd", "+zicsr", check_implicit_always},
   {"ssu64xl", "+zicsr", check_implicit_always},
+  {"ssdbltrp", "+zicsr", check_implicit_always},
 
   {"svade", "+zicsr", check_implicit_always},
   {"svadu", "+zicsr", check_implicit_always},
@@ -1446,6 +1448,7 @@ static struct riscv_supported_ext riscv_supported_std_s_ext[] =
   {"smepmp",		ISA_SPEC_CLASS_DRAFT,		1, 0, 0 },
   {"smrnmi",		ISA_SPEC_CLASS_DRAFT,		1, 0, 0 },
   {"smstateen",		ISA_SPEC_CLASS_DRAFT,		1, 0, 0 },
+  {"smdbltrp",		ISA_SPEC_CLASS_DRAFT,		1, 0, 0 },
   {"ssaia",		ISA_SPEC_CLASS_DRAFT,		1, 0, 0 },
   {"ssccptr",		ISA_SPEC_CLASS_DRAFT,		1, 0, 0 },
   {"sscsrind",		ISA_SPEC_CLASS_DRAFT,		1, 0, 0 },
@@ -1457,6 +1460,7 @@ static struct riscv_supported_ext riscv_supported_std_s_ext[] =
   {"sstvala",		ISA_SPEC_CLASS_DRAFT,		1, 0, 0 },
   {"sstvecd",		ISA_SPEC_CLASS_DRAFT,		1, 0, 0 },
   {"ssu64xl",		ISA_SPEC_CLASS_DRAFT,		1, 0, 0 },
+  {"ssdbltrp",		ISA_SPEC_CLASS_DRAFT,		1, 0, 0 },
   {"svade",		ISA_SPEC_CLASS_DRAFT,		1, 0, 0 },
   {"svadu",		ISA_SPEC_CLASS_DRAFT,		1, 0, 0 },
   {"svbare",		ISA_SPEC_CLASS_DRAFT,		1, 0, 0 },
